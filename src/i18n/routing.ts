c9d54df08bdba2a52db3fdf410c32fa30b6b0a5f
tobiask42 <tobiaskoch93@yahoo.de>
2025-07-11 12:38:17 +0200

    use of language cookies implemented

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,4 +1,19 @@
+// Path: src/i18n/routing.ts
+import { NextRequest } from "next/server";
+
 export const routing = {
-  locales: ['de', 'en'],
+  locales: ['en', 'de'],
   defaultLocale: 'de',
+
+  getLocale: (request: NextRequest): string => {
+    const cookieLocale = request.cookies.get('locale')?.value;
+
+    if (cookieLocale && ['en', 'de'].includes(cookieLocale)) {
+      return cookieLocale;
+    }
+
+    // rudimentärer Fallback via Header
+    const acceptLang = request.headers.get('accept-language');
+    return acceptLang?.startsWith('de') ? 'de' : 'en';
+  }
 };
