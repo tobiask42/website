1dba29f6d895f30584a7f3e35f39927af318f208
tobiask42 <tobiaskoch93@yahoo.de>
2026-05-20 13:52:51 +0200

    adjusted config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 // next.config.ts
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  // sorgt für stabilere Builds bei Deployments
+  generateBuildId: async () => {
+    return Date.now().toString();
+  },
+
+  // optional, aber gut für Debugging
+  productionBrowserSourceMaps: false,
+};
+
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
